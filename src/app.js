5de898ba810cd02933098343c0f1bc94235fc90f
GuilCavalcanti <58377529+GuilCavalcanti@users.noreply.github.com>
2020-08-20 00:28:46 +0100

    Done

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+// const { uuid } = require("uuidv4");
 
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +12,82 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repo = {
+    id: uuid(),
+    title,
+    url,
+    techs: [...techs],
+    likes: 0
+  }
+
+  repositories.push(repo)
+
+  return response.json(repo);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repoIndex = repositories.findIndex(repo => repo.id === id);
+  const oldRepo = repositories[repoIndex];
+
+  if (repoIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found.' })
+  }
+
+  repo = {
+    id: oldRepo.id,
+    title,
+    url,
+    techs,
+    likes: oldRepo.likes
+  }
+
+  repositories[repoIndex] = repo;
+  return response.json(repo);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repoIndex = repositories.findIndex(repo => repo.id === id);
+
+  if (repoIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found.' })
+  }
+
+  repositories.splice(repoIndex, 1);
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repoIndex = repositories.findIndex(repo => repo.id === id);
+
+  if (repoIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found.' });
+  }
+
+  const oldRepo = repositories[repoIndex];
+  let likes = oldRepo.likes;
+
+  likes++;
+
+  const repo = {
+    ...oldRepo,
+    likes
+  }
+  repositories[repoIndex] = repo;
+  return response.json(repo);
 });
 
 module.exports = app;
+
+
+
